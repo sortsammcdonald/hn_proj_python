ac24f90e7ecd2fa51d0c0aba62f1a1ef58e6f77f
Sam McDonald <sam@sammcdonald.me>
2023-10-09 19:18:13 +0200

    updatee

--- hn_api_proj.py
+++ hn_api_proj.py
@@ -1,18 +1,21 @@
 import requests
-
-class HN_User_Attributes:
-    def __init__(self, template, target, key):
+            
+class Gen_URLs():
+    def __init__(self, template, target):
         self.template = template
         self.target = target
+    def amend_url(self, snippet):
+        return self.template.replace(self.target, snippet)
+
+class HN_User_Attributes(Gen_URLs):
+    def __init__(self, template, target, key):
+        super().__init__(template, target)
         self.key = key
 
     def user_req(self):
         user_name = input("Enter your user name ")
         return user_name
-
-    def amend_url(self, snippet):
-        return self.template.replace(self.target, snippet)
-
+    
     def gen_json(self, snippet):
         url = self.amend_url(snippet)
         get_json = requests.get(url)
@@ -22,17 +25,17 @@ class HN_User_Attributes:
             print(f"Error: Received status code {get_json.status_code}")
             return None
             
-
     def extract_element(self, snippet):
         vals = self.gen_json(snippet)
-
         if vals is not None:
             if self.key in vals:
                 return vals[self.key]
             else:
                 print(f"Key '{self.key}' not found in JSON data.")
         else:
-            return None
+            return []
+
+
 
 
 def main():
@@ -41,26 +44,25 @@ def main():
         target = "<user>",
         key = "submitted"
     )
-    
-
-    
-
     snippet =  hn_user_attributes.user_req()
 
     data = hn_user_attributes.gen_json(snippet)
     section = hn_user_attributes.extract_element(snippet)
-
+    subsection = section[0:9]
+    new_urls = Gen_URLs(
+        template=  "https://news.ycombinator.com/item?id=<item>",
+        target= "<item>"
+    )
     
     
     if data is not None:
         print("Received JSON data:")
-        print(data)
-        print("Extract is")
-        print(section )
+        print(type(data))
+        for i in subsection:
+            print(new_urls.amend_url(snippet = str(i)))
     else:
-        print("Failed to retrieve JSON data.")
+            print("Failed to retrieve JSON data.")
 
 
 if __name__ == '__main__':
-
     main()
